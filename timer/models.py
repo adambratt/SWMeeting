49d7a16e005022d6d1e4fc2813101697b28447c5
Adam Bratt <adamjbratt@gmail.com>
2012-01-14 10:05:32 -0500

    installed timer app

--- timer/models.py
+++ timer/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Meeting(models.Model):
+    name=models.CharField(max_length=128)
+    create_ts=models.DateTimeField(auto_now_add=True)
+    def __unicode__(self):
+        return self.name
